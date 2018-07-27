419a812eeeec917a4a226da03897dd53cac0419e
KG <myloveqzk@163.com>
2018-07-27 10:26:27 +0800

    11

---
+++ ArtStar/Class/Friends/View/FriendsCell/DetaialCell/FriendsDetailCommentTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FriendsDetailCommentTableViewCell.h
+//  ArtStar
+//
+//  Created by abc on 2018/7/26.
+//  Copyright © 2018年 KG丿轩帝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsDetailCommentTableViewCell : UITableViewCell
+
+@end
---
+++ ArtStar/Class/Friends/View/FriendsCell/DetaialCell/FriendsDetailCommentTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  FriendsDetailCommentTableViewCell.m
+//  ArtStar
+//
+//  Created by abc on 2018/7/26.
+//  Copyright © 2018年 KG丿轩帝. All rights reserved.
+//
+
+#import "FriendsDetailCommentTableViewCell.h"
+
+@implementation FriendsDetailCommentTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
