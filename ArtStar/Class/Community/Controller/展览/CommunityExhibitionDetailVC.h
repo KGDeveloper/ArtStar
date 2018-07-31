2711ed7346e8ea1dc6c4eb6cd48039068a2a98ff
KG <31397060+KGDeveloper@users.noreply.github.com>
2018-07-31 17:08:53 +0800

    a

---
+++ ArtStar/Class/Community/Controller/展览/CommunityExhibitionDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  CommunityExhibitionDetailVC.h
+//  ArtStar
+//
+//  Created by 文艺星球 on 2018/7/31.
+//  Copyright © 2018年 KG丿轩帝. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface CommunityExhibitionDetailVC : BaseVC
+
+@end
---
+++ ArtStar/Class/Community/Controller/展览/CommunityExhibitionDetailVC.m
@@ -0,0 +1,37 @@
+//
+//  CommunityExhibitionDetailVC.m
+//  ArtStar
+//
+//  Created by 文艺星球 on 2018/7/31.
+//  Copyright © 2018年 KG丿轩帝. All rights reserved.
+//
+
+#import "CommunityExhibitionDetailVC.h"
+
+@interface CommunityExhibitionDetailVC ()
+
+@end
+
+@implementation CommunityExhibitionDetailVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
