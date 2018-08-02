8541e65f18f04e79740504cf959f2cf1cdacb7ab
KG <31397060+KGDeveloper@users.noreply.github.com>
2018-08-02 18:42:51 +0800

    q

--- ArtStar/Class/Community/View/音乐/头条/HeadlinesView.h
+++ ArtStar/Class/Community/View/音乐/头条/HeadlinesView.h
@@ -10,14 +10,42 @@
 
 @interface HeadlinesView : UIView
 
+/**
+ 点赞，评论，分享
+ */
+@property (nonatomic,copy) void(^selectCellWithAction)(NSString *title,NSString *ID);
+/**
+ 点击查看详情
+ */
 @property (nonatomic,copy) void(^pushViewController)(NSString *ID);
+/**
+ 刷新数据
+ */
 @property (nonatomic,copy) void(^requestNewData)(NSString *type);
+/**
+ 点击关闭新闻
+ */
 @property (nonatomic,copy) void(^closeNewsWithReson)(NSArray *resonArr,NSString *ID);
+/**
+ 新闻数据
+ */
 @property (nonatomic,copy) NSArray *dataArr;
+/**
+ 新闻最热5条数据
+ */
 @property (nonatomic,copy) NSArray *headerArr;
 
+/**
+ 显示新闻页面顶部最热5条新闻
+ */
 - (void)showHeaderView;
+/**
+ 隐藏新闻页面顶部最热5条新闻
+ */
 - (void)hideHeaderView;
+/**
+ 开始刷新页面
+ */
 - (void)starRefrash;
 
 @end
