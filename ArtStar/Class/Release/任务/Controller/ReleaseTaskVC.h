0c2999f9708c42e46e5f8c2b19874ddf72cb4f8a
KG <31397060+KGDeveloper@users.noreply.github.com>
2018-08-03 17:41:16 +0800

    修改布局以及开发设计思路

---
+++ ArtStar/Class/Release/任务/Controller/ReleaseTaskVC.h
@@ -0,0 +1,13 @@
+//
+//  ReleaseTaskVC.h
+//  ArtStar
+//
+//  Created by 文艺星球 on 2018/8/3.
+//  Copyright © 2018年 KG丿轩帝. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+@interface ReleaseTaskVC : BaseVC
+
+@end
---
+++ ArtStar/Class/Release/任务/Controller/ReleaseTaskVC.m
@@ -0,0 +1,38 @@
+//
+//  ReleaseTaskVC.m
+//  ArtStar
+//
+//  Created by 文艺星球 on 2018/8/3.
+//  Copyright © 2018年 KG丿轩帝. All rights reserved.
+//
+
+#import "ReleaseTaskVC.h"
+
+@interface ReleaseTaskVC ()
+
+@end
+
+@implementation ReleaseTaskVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
